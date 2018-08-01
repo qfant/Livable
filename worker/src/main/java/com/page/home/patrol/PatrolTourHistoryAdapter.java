824b3c0f72cdfafe403f2eff4a1233c237f7388c
liarman <287288189@qq.com>
2018-08-01 23:46:08 +0800

    bug fix

--- worker/src/main/java/com/page/home/patrol/PatrolTourHistoryAdapter.java
+++ worker/src/main/java/com/page/home/patrol/PatrolTourHistoryAdapter.java
@@ -1,38 +1,37 @@
 package com.page.home.patrol;
 
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.framework.adapter.utils.QSimpleAdapter;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.haolb.client.R;
-import com.page.home.patrol.PatrolTourResult.TourItem;
-
-import static com.page.home.patrol.PatrolTourHistoryResult.*;
-
 /**
  * Created by chenxi.cui on 2018/4/24.
  */
 
-class PatrolTourHistoryAdapter extends QSimpleAdapter<TourHistoryItem> {
-    public PatrolTourHistoryAdapter(Context context) {
+class PatrolTourHistoryAdapter extends BaseQuickAdapter<PatrolTourHistoryResult.TourHistoryItem,BaseViewHolder> {
+    public PatrolTourHistoryAdapter(int context) {
         super(context);
     }
 
     @Override
-    protected View newView(Context context, ViewGroup parent) {
-        return inflate(R.layout.pub_patrol_list_item_view, parent, false);
+    protected void convert(BaseViewHolder helper, PatrolTourHistoryResult.TourHistoryItem item) {
+        helper.setText(R.id.text_name, item.placename);
+//        helper.getView(R.id.text_time).setVisibility(View.GONE);
+        helper.setText(R.id.text_time, item.createtime);
     }
 
-    @Override
-    protected void bindView(View view, Context context, TourHistoryItem item, int position) {
-        TextView textName = (TextView) view.findViewById(R.id.text_name);
-        TextView textTime = (TextView) view.findViewById(R.id.text_time);
-        TextView if_arr = (TextView) view.findViewById(R.id.if_arr);
-        textName.setText(item.placename);
-        textTime.setText(item.createtime);
-        if_arr.setVisibility(View.GONE);
-//        textTime.setVisibility(View.GONE);
-    }
+//    @Override
+//    protected View newView(Context context, ViewGroup parent) {
+//        return inflate(R.layout.pub_patrol_list_item_view, parent, false);
+//    }
+//
+//    @Override
+//    protected void bindView(View view, Context context, TourHistoryItem item, int position) {
+//        TextView textName = (TextView) view.findViewById(R.id.text_name);
+//        TextView textTime = (TextView) view.findViewById(R.id.text_time);
+//        TextView if_arr = (TextView) view.findViewById(R.id.if_arr);
+//        textName.setText(item.placename);
+//        textTime.setText(item.createtime);
+//        if_arr.setVisibility(View.GONE);
+////        textTime.setVisibility(View.GONE);
+//    }
 }
